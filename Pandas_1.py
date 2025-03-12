33c1fb134812717ffba995b85801b1104dc0b61d
Sandesh Yesane <138008452+Sandesh30-cloud@users.noreply.github.com>
2025-03-12 20:04:33 +0530

    Pandas_1.py

---
+++ Pandas_1.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+pd.DataFrame()
+
+pd.Series()
+
+xyz.head(2) #output: gives first 3 rows of xyz table
+
+xyz.fillna() #fill the missing value
+
+xyz.describe() #gives statistics of data
+
